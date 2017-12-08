857d3ad023cdc3067403b9332f13a1df6ceefb04
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2017-12-08 13:47:13 +0530

    :fireworks: Fixed the template.

--- template/index.js
+++ template/index.js
@@ -1,19 +1,4 @@
-import Vue from './node_modules/vue/dist/vue.js';
-
-const app = new Vue({
-  el: '#app',
-  data: {
-    greeting: 'Welcome to your Vue.js app!',
-    docsURL: 'http://vuejs.org/guide/',
-    discordURL: 'https://chat.vuejs.org',
-    forumURL: 'http://forum.vuejs.org/',
-  },
-  methods: {
-    humanizeURL: function(url) {
-      return url.replace(/^https?:\/\//, '').replace(/\/$/, '');
-    },
-  },
-}).$mount();
+import Vue from 'vue';
 
 if (module.hot) {
   module.hot.accept();
