5b62b8650ca1525756f304de0223421c5f4130c8
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2017-12-10 20:29:39 +0530

    :sweat_smile: using CDN for Vue

--- template/index.js
+++ template/index.js
@@ -1,6 +1,19 @@
-import Vue from 'vue';
-
-window.Vue = Vue; 
+new Vue({
+  el: '#app',
+  data: {
+    greeting: 'Welcome to your Vue.js app!',
+    docsURL: 'http://vuejs.org/guide/',
+    discordURL: 'https://chat.vuejs.org',
+    forumURL: 'http://forum.vuejs.org/'
+  },
+  methods: {
+    humanizeURL: function (url) {
+      return url
+        .replace(/^https?:\/\//, '')
+        .replace(/\/$/, '')
+    }
+  }
+});
 
 if (module.hot) {
   module.hot.accept();
